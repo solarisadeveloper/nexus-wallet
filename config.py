3964174c6900836c61cd807e58ad2736438c50b5
solarisadeveloper <damon.french8@gmail.com>
2024-11-18 20:19:41 +1100

    Create config.py
    The file where you completely config your files.

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+BLOCKCYPHER_API_TOKEN = "YOUR_BLOCKCYPHER_API_TOKEN"  # Replace with your BlockCypher API token
+INFURA_PROJECT_ID = "YOUR_INFURA_PROJECT_ID"  # Replace with your Infura ID
+
+# Base URLs for different blockchains
+BTC_BASE_URL = "https://api.blockcypher.com/v1/btc/main"
+LTC_BASE_URL = "https://api.blockcypher.com/v1/ltc/main"
+ETH_BASE_URL = f"https://mainnet.infura.io/v3/{INFURA_PROJECT_ID}"
